f5df89b65a521de0740d9b724807c03af8f9654a
wanderer <56039464+wanderer-00@users.noreply.github.com>
2025-10-27 14:24:43 +0300

    Доработка кастомных китов

--- chest.js
+++ chest.js
@@ -1,7 +1,14 @@
 let chest = document.getElementsByClassName('chest')[0];
 
-for(i=0; i<27; i++){
-    chest.innerHTML += `
-<div class="cell"></div>
-    `;
-}
+// Если кит полностью заполнен одним предметом по стаку
+if (enderChest.length == 1) {
+    for (i = 0; i < 27; i++) {
+        chest.innerHTML += `<div class="cell" style="background-image: url(img/${enderChest[0][0]});">
+<div class="count">${enderChest[0][1]}</div>
+</div>`;}
+} else {
+    for (i = 0; i < 27; i++) {
+        chest.innerHTML += `<div class="cell" style="background-image: url(img/${enderChest[i][0]});">
+    <div class="count">${enderChest[i][1]}</div>
+</div>`;};
+};
